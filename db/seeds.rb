59e4b785b0863a1111f36be92432c722fa56beb1
Kevin Ndung'u <kevgathuku@gmail.com>
2017-11-29 15:41:16 +0300

    Add show seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+query = {
+  "api_key" => ENV['TMDB_API_KEY'],
+  "language" => 'en-US',
+  "sort_by" => "popularity.desc"
+}
+
+response = HTTParty.get(
+  "https://api.themoviedb.org/3/discover/tv",
+  :query => query
+)
+
+response.parsed_response['results'].each { |fetched_show|
+  Show.find_or_create_by(tmdb_id: fetched_show['id']) do |show|
+    show.name = fetched_show['name']
+    show.overview = fetched_show['overview']
+    show.tmdb_id = fetched_show['id']
+    show.tmdb_data = fetched_show
+  end
+}
