ff1c851ee9a8ed04ccfa3a238ce770eddabaa106
Kevin Ndung'u <kevgathuku@gmail.com>
2017-11-29 16:09:48 +0300

    Add placeholder content styled with tachyons

--- app/javascript/Main.elm
+++ app/javascript/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
-import Html exposing (Html, h1, text)
-import Html.Attributes exposing (style)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 -- MODEL
@@ -26,10 +26,44 @@ init =
 
 view : Model -> Html Message
 view model =
-    -- The inline style is being used for example purposes in order to keep this example simple and
-    -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-    h1 [ style [ ( "display", "flex" ), ( "justify-content", "center" ) ] ]
-        [ text "Hello Elm!" ]
+    article []
+        [ h2 [ class "f3 fw4 pa3 mv0" ]
+            [ text "Albums" ]
+        , div [ class "cf pa2" ]
+            [ div [ class "fl w-50 w-25-m w-20-l pa2" ]
+                [ a [ href "https://geo.itunes.apple.com/us/album/blonde/id1146195596?at=1l3vqFJ&mt=1&app=music", class "db link dim tc" ]
+                    [ img [ src "http://is4.mzstatic.com/image/thumb/Music62/v4/93/8f/75/938f7536-0188-f9ba-4585-0a77ceaebf0a/source/400x40000bb.png", alt "Frank Ocean Blonde Album Cover", class "w-100 db outline black-10" ]
+                        []
+                    , dl [ class "mt2 f6 lh-copy" ]
+                        [ dt [ class "clip" ]
+                            [ text "Title" ]
+                        , dd [ class "ml0 black truncate w-100" ]
+                            [ text "Blonde" ]
+                        , dt [ class "clip" ]
+                            [ text "Artist" ]
+                        , dd [ class "ml0 gray truncate w-100" ]
+                            [ text "Frank Ocean" ]
+                        ]
+                    ]
+                ]
+            , div [ class "fl w-50 w-25-m w-20-l pa2" ]
+                [ a [ href "https://geo.itunes.apple.com/us/album/everybody-looking/id1135576036?at=1l3vqFJ&mt=1&app=music", class "db link dim tc" ]
+                    [ img [ src "http://is4.mzstatic.com/image/thumb/Music30/v4/9b/2e/e1/9b2ee13e-35fd-0cc1-d203-e65b4beafc7f/source/400x40000bb.png", alt "Everybody Looking - Album Cover", class "w-100 db outline black-10" ]
+                        []
+                    , dl [ class "mt2 f6 lh-copy" ]
+                        [ dt [ class "clip" ]
+                            [ text "Title" ]
+                        , dd [ class "ml0 black truncate w-100" ]
+                            [ text "Everybody Looking" ]
+                        , dt [ class "clip" ]
+                            [ text "Artist" ]
+                        , dd [ class "ml0 gray truncate w-100" ]
+                            [ text "Gucci Mane" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
 
 
 
--- app/javascript/packs/hello_elm.js
+++ app/javascript/packs/hello_elm.js
@@ -2,11 +2,13 @@
 // head of your layout file, like app/views/layouts/application.html.erb.
 // It will render "Hello Elm!" within the page.
 
-import Elm from '../Main'
+import Elm from "../Main";
+import "tachyons/css/tachyons.min.css";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const target = document.createElement('div')
+document.addEventListener("DOMContentLoaded", () => {
+  const target = document.getElementById("root");
 
-  document.body.appendChild(target)
-  Elm.Main.embed(target)
-})
+  document.body.appendChild(target);
+  Elm.Main.embed(target);
+});
+
