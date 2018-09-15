72ea4536280814d805c0497dbacd0ab2d7348cc3
Kevin Ndung'u <kevgathuku@gmail.com>
2018-09-15 12:55:25 +0300

    Avoid running seeds when the TMDB API key is unavailable

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-query = {
-  "api_key" => ENV['TMDB_API_KEY'],
-  "language" => 'en-US',
-  "sort_by" => "popularity.desc"
-}
+unless ENV.has_key? "TMDB_API_KEY" or ENV.fetch === ""
+  query = {
+    "api_key" => ENV["TMDB_API_KEY"],
+    "language" => "en-US",
+    "sort_by" => "popularity.desc",
+  }
 
-response = HTTParty.get(
-  "https://api.themoviedb.org/3/discover/tv",
-  :query => query
-)
+  response = HTTParty.get(
+    "https://api.themoviedb.org/3/discover/tv",
+    :query => query,
+  )
 
-SAVED_SHOW_PROPERTIES = %w( name overview id )
+  SAVED_SHOW_PROPERTIES = %w( name overview id )
 
-response.parsed_response['results'].each {|fetched_show|
-  Show.find_or_create_by(tmdb_id: fetched_show['id']) do |show|
-    show.name = fetched_show['name']
-    show.overview = fetched_show['overview']
-    show.tmdb_id = fetched_show['id']
-    show.tmdb_data = fetched_show.reject {|k,v| SAVED_SHOW_PROPERTIES.include?(k)}
-  end
-}
+  response.parsed_response["results"].each { |fetched_show|
+    Show.find_or_create_by(tmdb_id: fetched_show["id"]) do |show|
+      show.name = fetched_show["name"]
+      show.overview = fetched_show["overview"]
+      show.tmdb_id = fetched_show["id"]
+      show.tmdb_data = fetched_show.reject { |k, v| SAVED_SHOW_PROPERTIES.include?(k) }
+    end
+  }
+end
