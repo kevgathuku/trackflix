133f6313013395d2a9109a7e8a68a0ab1dcd3755
Kevin Ndung'u <kevgathuku@gmail.com>
2018-09-17 10:02:34 +0300

    Implement getting user data from trakt

--- api/shows.ts
+++ api/shows.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import * as qs from "qs";
 
+import { trakt } from "./trakt";
+
 require("dotenv").config();
 
 const BASE_API_URL = "https://api.themoviedb.org/3";
@@ -9,7 +11,7 @@ const requiredParams = qs.stringify({
   api_key: process.env.TMDB_API_KEY
 });
 
-const discoverTvShows = () => {
+export const discoverTvShows = () => {
   return axios
     .get(`${BASE_API_URL}/discover/tv?${requiredParams}`)
     .then(response => {
@@ -18,8 +20,39 @@ const discoverTvShows = () => {
     })
     .catch(error => {
       console.error(error);
-      return { error: error };
+      return { error };
     });
 };
 
-module.exports = { discoverTvShows };
+export const traktAuth = () => {
+  return { authURL: trakt.get_url() };
+};
+
+export const traktRedirect = request => {
+  const { code, state } = request.query;
+  console.log("code", code);
+  console.log("state", state);
+
+  // TODO: Return actual error response with correct statusCode
+  if (!code) return { error: "Invalid data" };
+
+  return trakt
+    .exchange_code(code, state)
+    .then(result => {
+      console.log("result", result);
+      // contains tokens & session information
+      // API can now be used with authorized requests
+      return trakt.sync
+        .watched({
+          type: "shows"
+        })
+        .then(watchedShows => {
+          console.log("watchedShows", watchedShows);
+          return { watchedShows };
+        });
+    })
+    .catch(error => {
+      console.error(error);
+      return { error };
+    });
+};
--- api/trakt.ts
+++ api/trakt.ts
@@ -1,14 +1,15 @@
-const Trakt = require('trakt.tv');
+const Trakt = require("trakt.tv");
 
 require("dotenv").config();
 
 const options = {
   client_id: process.env.TRAKT_CLIENT_ID,
   client_secret: process.env.TRAKT_CLIENT_SECRET,
-  redirect_uri: null,   // defaults to 'urn:ietf:wg:oauth:2.0:oob'
-  api_url: null,        // defaults to 'https://api.trakt.tv'
-  useragent: null,      // defaults to 'trakt.tv/<version>'
-  pagination: true      // defaults to false, global pagination (see below)
+  redirect_uri: process.env.REDIRECT_URI,
+  api_url: null, // defaults to 'https://api.trakt.tv'
+  useragent: null, // defaults to 'trakt.tv/<version>'
+  pagination: true, // defaults to false, global pagination
+  debug: true
 };
 
 export const trakt = new Trakt(options);
